52a25cc26c53f7c3cb60f3168dd684b3ef8e5d7d
Shrey <shrma.shray@gmail.com>
2022-09-18 21:11:19 +0530

    login page modified 9:11 18/9/2022

--- flutter_front_end/lib/signup_page.dart
+++ flutter_front_end/lib/signup_page.dart
@@ -12,57 +12,68 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[200],
-      body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                      padding: EdgeInsets.symmetric(horizontal: 30.0),
-                      child: Image.asset('assets/iit_jammu_logo.png')
-                ),
-                SizedBox(height: 50,),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Text("CONNECT WITH OFFICIAL GOOGLE ID",
-                    style: GoogleFonts.roboto(
-                      fontSize: 15,
-                      color: Colors.blue[900],
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.white,
-                  ),
-                  width: 300,
-                  height: 70,
-                 // color: Colors.white,
-                  child: Image.asset('assets/google.png'),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                    padding: EdgeInsets.symmetric(horizontal: 42,vertical: 10),
-                    child: Image.asset('assets/campus.jpg'),
-                ),
-                SizedBox(height: 10,),
-                Center(
-                  child: Text("IIT JAMMU OFFICIAL TIME TABLE APP",
-                    style: GoogleFonts.roboto(
-                      fontSize: 18,
-                      color: Colors.blue[900],
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
 
-              ],
-            ),
+     // backgroundColor: Colors.blue[200],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/eberhard.jpg'),
+            fit: BoxFit.cover,
           ),
+        ),
+        child: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                        padding: EdgeInsets.symmetric(horizontal: 30.0),
+                        child: Image.asset('assets/iit_jammu_logo.png')
+                  ),
+                  SizedBox(height: 50,),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text("CONNECT WITH OFFICIAL GOOGLE ID",
+                      style: GoogleFonts.roboto(
+                        fontSize: 15,
+                        color: Colors.blue[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white,
+                    ),
+                    width: 300,
+                    height: 70,
+                    child: TextButton(
+                      onPressed: (){},
+                      child: Image.asset('assets/google.png'),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                /*  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 42,vertical: 10),
+                      child: Image.asset('assets/campus.jpg'),
+                  ), */
+               //   SizedBox(height: 10,),
+                  Center(
+                    child: Text("IIT JAMMU OFFICIAL TIME TABLE APP",
+                      style: GoogleFonts.roboto(
+                        fontSize: 18,
+                        color: Colors.blue[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+        ),
       ),
     );
   }
