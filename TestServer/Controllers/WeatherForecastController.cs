73ded19758966d67228f582f6ed5166f917ecc07
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-09-18 13:29:31 +0530

    JWT services and extensions configured.

--- TestServer/Controllers/WeatherForecastController.cs
+++ TestServer/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TestServer.Controllers
@@ -17,7 +18,7 @@ namespace TestServer.Controllers
         {
             _logger = logger;
         }
-
+        [Authorize]
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
