9c6666d4e2eaac0ea8cefda046893dc53eb4593f
Shrey <shrma.shray@gmail.com>
2022-11-02 16:18:02 +0530

    logout and time table example implemented 2/11/2022

--- flutter_front_end/lib/google_signin_api.dart
+++ flutter_front_end/lib/google_signin_api.dart
@@ -4,5 +4,7 @@ class GoogleSignInAPI {
   static final _googleSignIn = GoogleSignIn();
 
   static Future<GoogleSignInAccount?> login()=> _googleSignIn.signIn();
+
+  static Future logout() => _googleSignIn.disconnect();
 }
 
--- flutter_front_end/lib/signedin_page.dart
+++ flutter_front_end/lib/signedin_page.dart
@@ -13,13 +13,239 @@ class SignedInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
-      title: Text("User"),
+      title: Text(user.displayName!),
       centerTitle: true,
+      backgroundColor: Colors.black12,
+      actions: [
+        TextButton(
+            onPressed: () async {
+              await GoogleSignInAPI.logout();
+
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => SignupPage(),
+              ));
+            },
+            child: const Text('Logout',
+              style: TextStyle(
+                  color: Colors.black
+              ),
+            ),
+        )
+      ],
     ),
     body: Container(
-      child: CircleAvatar(
-        radius: 40,
-        backgroundImage: NetworkImage(user.photoUrl!),
+      child: PageView(
+        children: [
+          Column(
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Wednusday  12/10/2022'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot A    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot B    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot C    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot D    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+            ],
+          ),
+
+
+          Column(
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Thursday  13/10/2022'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot A    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot B    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot C    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot D    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+            ],
+          ),
+
+
+
+          Column(
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Friday  14/10/2022'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot A    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot B    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot C    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('Slot D    Details'),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+            ],
+          ),
+        ],
       ),
     ),
     );
